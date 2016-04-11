8d2d2ce609e3a08035d19123b57672b2d561488e
Michael Wheatley <michaelvwheatley@gmail.com>
2016-04-11 11:46:09 -0500

    pooling

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -3,28 +3,13 @@ angular.module('myApp.services',[])
 .factory('Service', function($http, $window, $location){
 	var service = {};
 
-  // service.login = function(user){
-  //   return $http({
-  //     method: 'POST',
-  //     url: '/admin/login',
-  //     data: user
-  //   })
-  //   .then(function(res){
-  //     localStorage.setItem('auth', true);
-  //     return res.data;
-  //   })
-  //   .catch(function(error){
-  //     console.error('Error logging into the ADMIN page:', error);
-  //   })
-  // };
-
   service.getData = function(){
     return $http({
       method:'GET',
       url: '/data'
     })
     .then(function(resp) {
-      return resp.data
+      return resp.data || [];
     })
     .catch(function(error) {
       console.error(error);
@@ -37,7 +22,7 @@ angular.module('myApp.services',[])
       url: '/data2'
     })
     .then(function(resp) {
-      return resp.data
+      return resp.data || [];
     })
     .catch(function(error) {
       console.error(error);
--- server/index.js
+++ server/index.js
@@ -6,58 +6,64 @@ var Path = require('path');
 var mysql = require('mysql');
 var env       = process.env.NODE_ENV || "development";
 if (env === "development") {
-	var con = mysql.createConnection({
+	var pool = mysql.createPool({
 	  host: "localhost",
 	  user: "root",
 	  password: null,
 	  database: "givepulse_test"
 	});
 } else {
-	var con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+	var pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
 }
-
-con.connect(function(err){
-  if(err){
-    console.log('Error connecting to givepulse_test');
-    return;
-  }
-  console.log('Connection established to givepulse_test');
-});
 
 var assetFolder = Path.resolve(__dirname, '../client/');
 app.use( express.static(assetFolder) );
 app.use( bodyParser.json() );
 
 app.get('/data', function(req, res) {
-	var query = 'select sum(duration_hours) hours, groups.type' + 
-							' from groups, events, impacts' + 
-							' where groups.id = events.group_id' +
-							' and events.id = impacts.event_id' +
-							' group by groups.type';
-	console.log('query', query);
-	con.query(query, function(err, rows) {
-		res.send(rows);
-	})
-})
+	pool.getConnection(function(err, connection){
+		if(err) {
+			console.log('Error connecting to givepulse_test:', err);
+    	return;
+		}
+		var query = 'select sum(duration_hours) hours, groups.type' + 
+								' from groups, events, impacts' + 
+								' where groups.id = events.group_id' +
+								' and events.id = impacts.event_id' +
+								' group by groups.type';
+		console.log('query', query);
+		connection.query(query, function(err, rows) {
+			connection.release();
+			res.send(rows);
+		});
+	});
+});
 
 app.get('/data2', function(req, res) {
-	var query2 = 'select skills_by_event.skill, events, users from' + 
-							' (select skills.skill, count(events.id) events' +
-							' from skills, event_skills, events' +
-							' where event_skills.event_id = events.id' +
-							' and event_skills.skill_id = skills.id' +
-							' group by skills.skill) skills_by_event,' +
-							' (select skills.skill, count(users.id) users' +
-							' from skills, user_skills, users' +
-							' where skills.id = user_skills.skill_id' +
-							' and users.id = user_skills.user_id' +
-							' group by skills.skill) skills_by_users' +
-							' where skills_by_event.skill = skills_by_users.skill';
-	console.log('query2', query2);
-	con.query(query2, function(err, rows) {
-		res.send(rows);
-	})
-})
+	pool.getConnection(function(err, connection){
+		if(err) {
+			console.log('Error connecting to givepulse_test:', err);
+	  	return;
+		}
+		var query2 = 'select skills_by_event.skill, events, users from' + 
+								' (select skills.skill, count(events.id) events' +
+								' from skills, event_skills, events' +
+								' where event_skills.event_id = events.id' +
+								' and event_skills.skill_id = skills.id' +
+								' group by skills.skill) skills_by_event,' +
+								' (select skills.skill, count(users.id) users' +
+								' from skills, user_skills, users' +
+								' where skills.id = user_skills.skill_id' +
+								' and users.id = user_skills.user_id' +
+								' group by skills.skill) skills_by_users' +
+								' where skills_by_event.skill = skills_by_users.skill';
+		console.log('query2', query2);
+		connection.query(query2, function(err, rows) {
+			connection.release();
+			res.send(rows);
+		});
+	});
+});
 
 
 // Start the server!
